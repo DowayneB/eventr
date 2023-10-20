eeee3009f9f1d56c58bfc380c3ff3c56278d0af2
Dowayne Breedt <dowayne.b@afrihost.com>
2023-10-20 20:27:59 +0200

    Check what these people are posting

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,6 +12,15 @@ class IndexController extends AbstractController
     #[Route('/index', name: 'app_index')]
     public function index(): Response
     {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+    #[Route('/mfn/API/porta/backspace/getProductList/voice', name: 'test')]
+    public function test(Request $request): Response
+    {
+        file_put_contents("/mfn.txt", json_encode($request->query->all()));
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
