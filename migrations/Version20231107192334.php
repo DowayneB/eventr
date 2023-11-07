7a85011b694dc93de283c0cf51961437602626c4
Dowayne Breedt <dowayne.b@afrihost.com>
2023-11-07 21:25:07 +0200

    Migrations

---
+++ migrations/Version20231107192334.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231107192334 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE event ADD user_id INT NOT NULL, ADD event_type_id INT NOT NULL');
+        $this->addSql('ALTER TABLE event ADD CONSTRAINT FK_3BAE0AA7A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id)');
+        $this->addSql('ALTER TABLE event ADD CONSTRAINT FK_3BAE0AA7401B253C FOREIGN KEY (event_type_id) REFERENCES event_type (id)');
+        $this->addSql('CREATE INDEX IDX_3BAE0AA7A76ED395 ON event (user_id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_3BAE0AA7401B253C ON event (event_type_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE event DROP FOREIGN KEY FK_3BAE0AA7A76ED395');
+        $this->addSql('ALTER TABLE event DROP FOREIGN KEY FK_3BAE0AA7401B253C');
+        $this->addSql('DROP INDEX IDX_3BAE0AA7A76ED395 ON event');
+        $this->addSql('DROP INDEX UNIQ_3BAE0AA7401B253C ON event');
+        $this->addSql('ALTER TABLE event DROP user_id, DROP event_type_id');
+    }
+}
