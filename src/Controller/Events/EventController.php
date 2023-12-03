9129cb959748d927979a746323610cff23dd29bc
Dowayne Breedt <dowayne.b@afrihost.com>
2023-12-03 22:28:27 +0200

    Endpoint to create events

--- src/Controller/Events/EventController.php
+++ src/Controller/Events/EventController.php
@@ -5,7 +5,9 @@ namespace App\Controller\Events;
 use App\Controller\EventrController;
 use App\Entity\Event;
 use App\Entity\EventType;
-use JMS\Serializer\SerializerBuilder;
+use App\Manager\EventManager;
+use App\Manager\EventTypeManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,11 +25,35 @@ class EventController extends EventrController
         );
     }
 
+    #[Route('', name: "api_event_create_post", methods: ["POST"])]
+    public function testApi(EventTypeManager $eventTypeManager, Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        if (!$request->get('event_type_id')) {
+            throw new \Exception('Event type ID must be supplied');
+        }
+
+        $eventType = $eventTypeManager->getEventType($request->get('event_type_id'));
+
+        if (!$eventType instanceof EventType) {
+            throw new \Exception("No event type found with given ID");
+        }
+
+        if (!$request->get('event_date')) {
+            throw new \Exception("Event date must be supplied");
+        }
+
+        if (!$request->get('rsvp_date')) {
+            throw new \Exception("RSVP date must be supplied");
+        }
+
         $event = new Event();
         $event->setEventType($eventType);
-        $event->setEventDate(new \DateTime());
+        $event->setEventDate(new \DateTime($request->get('event_date')));
         $event->setUser($this->getUser());
-        $event->setRsvpDate(new \DateTime());
+        $event->setRsvpDate(new \DateTime($request->get('rsvp_date')));
+
+        $entityManager->persist($event);
+        $entityManager->flush();
 
         return $this->makeSerializedResponse(
             [
