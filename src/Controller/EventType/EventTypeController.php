351f22235c6beeb42f60d6069e46fdf1f694976b
Dowayne Breedt <dowayne.b@afrihost.com>
2024-02-25 14:12:37 +0200

    event types endpoint

---
+++ src/Controller/EventType/EventTypeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\EventType;
+
+use App\Controller\EventrController;
+use App\Manager\EventTypeManager;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(
+    "/api/event-type",
+    name: "event_type_main"
+)]class EventTypeController extends EventrController
+{
+    #[Route('/', name: 'app_event_type')]
+    public function index(EventTypeManager $eventTypeManager): Response
+    {
+        return $this->makeSerializedResponse([
+            'event-type' => $eventTypeManager->getEventTypes()
+        ]);
+    }
+}
