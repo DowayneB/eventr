3418ecb578b9b15903ad61a98884ef70eea2497c
Dowayne Breedt <dowayne.b@afrihost.com>
2024-02-26 13:13:48 +0200

    Add validation methods for cell num and email

---
+++ src/Helper/ValidationHelper.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Helper;
+
+class ValidationHelper
+{
+    public static function validateEmailAddress(string $email): bool
+    {
+        return true;
+    }
+
+    public static function validateCellNumber(string $number): bool
+    {
+        return true;
+    }
+}
