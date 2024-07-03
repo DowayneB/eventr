22577f16391d57e50b9abb843e178353d0c0f1c3
Dowayne Breedt <dowayne.b@afrihost.com>
2024-07-03 14:56:23 +0200

    New invitation entity

---
+++ src/Entity/Invitation.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InvitationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation\Timestampable;
+
+#[ORM\Entity(repositoryClass: InvitationRepository::class)]
+class Invitation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToOne(inversedBy: 'invitation', cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Event $event = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $html = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Timestampable(on: 'create')]
+    private \DateTimeInterface $created_at;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Timestampable(on: 'update')]
+    private \DateTimeInterface $updated_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEventId(): ?Event
+    {
+        return $this->event_id;
+    }
+
+    public function setEventId(Event $event_id): static
+    {
+        $this->event_id = $event_id;
+
+        return $this;
+    }
+
+    public function getHtml(): ?string
+    {
+        return $this->html;
+    }
+
+    public function setHtml(string $html): static
+    {
+        $this->html = $html;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): void
+    {
+        $this->created_at = $created_at;
+    }
+
+    public function getUpdatedAt(): \DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updated_at): void
+    {
+        $this->updated_at = $updated_at;
+    }
+
+
+}
