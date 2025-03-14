280f328ffd4d62ef33a77f5e1f8bcf704719b6e9
Dowayne Breedt <dowayne.b@afrihost.com>
2025-03-14 14:17:51 +0200

    Method to create a user profile

--- src/Manager/UserProfileManager.php
+++ src/Manager/UserProfileManager.php
@@ -2,7 +2,25 @@
 
 namespace App\Manager;
 
+use App\Entity\UserProfile;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
 class UserProfileManager
 {
-
+    public function createUserProfile(
+        UserInterface $user,
+        string $name,
+        string $surname,
+        string $cellNumber
+    ): UserProfile {
+        $userProfile = $user->getUserProfile();
+        if (!$userProfile instanceof UserProfile) {
+            $userProfile = new UserProfile();
+        }
+        return $userProfile->setUser($user)
+            ->setName($name)
+            ->setSurname($surname)
+            ->setCellNumber($cellNumber);
+    }
 }
