0a4669794bb47ad41a325de42de2a598bfa219e1
Dowayne Breedt <dowayne.b@afrihost.com>
2025-03-14 14:17:24 +0200

    Create controller to get and update user profiles

---
+++ src/Controller/UserProfile/UserProfileController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\UserProfile;
+
+use App\Controller\EventrController;
+use App\Manager\UserProfileManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[Route('/api/user-profile')]
+final class UserProfileController extends EventrController
+{
+    #[Route(null, name: 'app_user_profile', methods: ["GET"])]
+    public function index(): Response
+    {
+        return $this->makeSerializedResponse([
+                'user_profile' => $this->getUser()->getUserProfile()
+        ]);
+    }
+
+    #[Route(null, name: 'app_user_profile_create', methods: ["POST"])]
+    public function create(Request $request,UserInterface $user, UserProfileManager $userProfileManager, EntityManagerInterface $entityManager): Response
+    {
+        $userProfile = $userProfileManager->createUserProfile(
+            $this->getUser(),
+            $this->get($request, 'name'),
+            $this->get($request, 'surname'),
+            $this->get($request,'cell_number')
+        );
+
+        $user->setUserProfile($userProfile);
+
+        $entityManager->persist($userProfile);
+        $entityManager->flush($user);
+
+        return $this->makeSerializedResponse([
+            'user_profile' => $userProfile
+        ]);
+    }
+}
