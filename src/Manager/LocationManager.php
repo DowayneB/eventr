44cfeb96e66f2bad45fb7b940597a50b258e2040
Dowayne Breedt <dowayne.b@afrihost.com>
2025-07-25 00:01:16 +0200

    method to find location

--- src/Manager/LocationManager.php
+++ src/Manager/LocationManager.php
@@ -4,22 +4,34 @@ namespace App\Manager;
 
 use App\Entity\Location;
 use App\Entity\LocationType;
+use App\Repository\LocationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
 use function PHPUnit\Framework\returnArgument;
 
 class LocationManager
 {
+    private LocationRepository $locationRepository;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(
+        EntityManagerInterface $entityManager
+    ) {
+        $this->entityManager = $entityManager;
+        $this->locationRepository = $this->getEntityManager()->getRepository(Location::class);
+    }
 
     public function createLocation(
         LocationType $locationType,
-        string $country,
-        string $province,
-        string $city,
-        string $suburb,
-        string $streetName,
-        string $streetNumber,
-        string $latitude,
-        string $longitude,
-        string $placeId
+        string       $country,
+        string       $province,
+        string       $city,
+        string       $suburb,
+        string       $streetName,
+        string       $streetNumber,
+        string       $latitude,
+        string       $longitude,
+        string       $placeId
     ): Location {
         return (new Location())
             ->setLocationType($locationType)
@@ -32,5 +44,20 @@ class LocationManager
             ->setLatitude($latitude)
             ->setLongitude($longitude)
             ->setPlaceId($placeId);
+    }
+
+    public function findLocation(int $id): ?Location
+    {
+        return $this->getLocationRepository()->find($id);
+    }
+
+    private function getEntityManager(): EntityManagerInterface
+    {
+        return $this->entityManager;
+    }
+
+    private function getLocationRepository(): EntityRepository
+    {
+        return $this->locationRepository;
     }
 }
