e9a59d911d335186c25a55a4d99241a874834ed1
Dowayne Breedt <dowayne.b@afrihost.com>
2025-07-25 21:48:19 +0200

    Add endpoint to get event type by ID

--- src/Controller/EventType/EventTypeController.php
+++ src/Controller/EventType/EventTypeController.php
@@ -4,6 +4,7 @@ namespace App\Controller\EventType;
 
 use App\Controller\EventrController;
 use App\Manager\EventTypeManager;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,11 +14,21 @@ use Symfony\Component\Routing\Annotation\Route;
 )]
 class EventTypeController extends EventrController
 {
-    #[Route('', name: 'app_event_type')]
+    #[Route('', name: 'api_event_types')]
     public function index(EventTypeManager $eventTypeManager): Response
     {
         return $this->makeSuccessfulResponse([
             'event-type' => $eventTypeManager->getEventTypes()
+        ]);
+    }
+
+    #[Route('/{event-type-id}', name: 'api_event_type')]
+    public function eventType(
+        EventTypeManager $eventTypeManager,
+        Request          $request,
+    ): Response {
+        return $this->makeSuccessfulResponse([
+            'event-type' => $eventTypeManager->getEventType($this->get($request, $request->attributes->get('event-type-id'))),
         ]);
     }
 }
