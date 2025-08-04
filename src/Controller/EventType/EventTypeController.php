b5a0f58a8f3ca4c17f226e6525858f01a529d040
Dowayne Breedt <dowayne.b@afrihost.com>
2025-08-04 22:36:55 +0200

    Remove EventrController from EventTypeController

--- src/Controller/EventType/EventTypeController.php
+++ src/Controller/EventType/EventTypeController.php
@@ -5,6 +5,9 @@ namespace App\Controller\EventType;
 use App\Controller\EventrController;
 use App\Entity\EventType;
 use App\Manager\EventTypeManager;
+use JMS\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,34 +16,45 @@ use Symfony\Component\Routing\Annotation\Route;
     "/api/event-type",
     name: "event_type_main"
 )]
-class EventTypeController extends EventrController
+class EventTypeController extends AbstractController
 {
     #[Route('', name: 'api_event_types')]
-    public function index(EventTypeManager $eventTypeManager): Response
+    public function index(
+        EventTypeManager $eventTypeManager,
+        SerializerInterface $serializer
+    ): JsonResponse
     {
-        return $this->makeSuccessfulResponse([
-            'event-type' => $eventTypeManager->getEventTypes()
-        ]);
+        return new JsonResponse(
+            $serializer->serialize(['event-types' => $eventTypeManager->getEventTypes()], 'json'),
+            Response::HTTP_OK,
+            [],true
+        );
     }
 
-    #[Route('/{event-type-id}', name: 'api_event_type')]
+    #[Route('/{eventTypeId}', name: 'api_event_type')]
     public function eventType(
         int              $eventTypeId,
         EventTypeManager $eventTypeManager,
+        SerializerInterface $serializer
     ): Response {
 
         $eventType = $eventTypeManager->getEventType($eventTypeId);
 
         if (!$eventType instanceof EventType) {
-            return $this->makeValidationFailureResponse(
-                'event_id',
-                "Guest with ID {$eventTypeId} not found.",
-                Response::HTTP_NOT_FOUND
+            return new JsonResponse(
+                $serializer->serialize(['errors' => [
+                    'field' => 'eventTypeId',
+                    'message' => "Event type with id {$eventTypeId} not found",
+                ]], 'json'),
+                Response::HTTP_NOT_FOUND,
+                [],true
             );
         }
 
-        return $this->makeSuccessfulResponse([
-            'event-type' => $eventType,
-        ]);
+        return new JsonResponse(
+            $serializer->serialize(['event-type' => $eventType], 'json'),
+            Response::HTTP_OK,
+            [],true
+        );
     }
 }
